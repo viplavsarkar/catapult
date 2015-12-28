baa0401398933d7a8fdcfdfeb324fc793d422e71
dinkarthakur <dinkarthakur@192.168.17.178>
2015-12-28 20:24:41 +0530

    Request Parameter validation done

--- parent/CompositeService/UserCompositeService/src/main/java/com/wiziq/services/cs/user/controller/User.java
+++ parent/CompositeService/UserCompositeService/src/main/java/com/wiziq/services/cs/user/controller/User.java
@@ -2,14 +2,16 @@ package com.wiziq.services.cs.user.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.wiziq.services.cs.user.component.UserSignUp;
 import com.wiziq.services.cs.user.service.FeignService;
 
 @RestController
@@ -17,24 +19,14 @@ public class User {
 
 	private final AtomicLong counter = new AtomicLong();
 
-   @Autowired
-    private FeignService feignService;
-	
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String feignClient() {
-		System.out.println(counter.incrementAndGet() + ". ServiceBController.feignClient");
-	    return feignService.feignClient();
-	}
+	@Autowired
+	private FeignService feignService;
 
-	 @RequestMapping(value = "/test", method = RequestMethod.GET)
-	 @HystrixCommand(fallbackMethod = "defaultProductComposite")
-	 public ResponseEntity<String> test() {
-	 System.out.println(counter.incrementAndGet() + ". Userconposite Servic Test");
-	 return new ResponseEntity<String>("hello", HttpStatus.OK);
-	 }
-	 public ResponseEntity<String> defaultProductComposite() {
-	
-	 return new ResponseEntity<String>("", HttpStatus.BAD_GATEWAY);
-	 }
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String feignClient(@Valid @RequestBody UserSignUp userSignUp, BindingResult bindResult){
+		System.out.println(counter.incrementAndGet() + ". ServiceBController.feignClient");
+		//if(bindResult.hasErrors()) return bindResult.getAllErrors();
+		return feignService.feignClient();
+	}
 
 }
