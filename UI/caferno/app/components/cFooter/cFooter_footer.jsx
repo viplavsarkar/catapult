4d1753d19542fc8d4cd171b1b038c48a438919fb
Biplab Sarkar <viplavsarkar@yahoo.co.in>
2016-03-19 13:24:40 +0530

    commiting newly created files

---
+++ UI/caferno/app/components/cFooter/cFooter_footer.jsx
@@ -0,0 +1,76 @@
+/*if (typeof window === 'undefined') {
+    var React = require('react');
+    var ReactIntl = require('react-intl');
+    var IntlMixin = ReactIntl.IntlMixin;
+    var FormattedMessage = ReactIntl.FormattedMessage;
+} else {
+    var React = window.React;
+}
+*/
+
+var isNode = typeof module !== 'undefined' && module.exports
+  , React = isNode ? require('react') : window.React
+  , ReactDOM = isNode ? require('react-dom') : window.ReactDOM
+  , ReactIntl = isNode ? require('react-intl') : window.ReactIntl;
+
+    
+var IntlMixin = ReactIntl.IntlMixin;
+var FormattedMessage = ReactIntl.FormattedMessage;
+var FormattedDate = ReactIntl.FormattedDate;
+
+var Footer = React.createClass({
+    mixins: [IntlMixin],
+    render: function () {
+        var data = this.props.data;
+
+        return (
+            <footer id="pageFooter" className="moduleBody">
+                <div className="moduleWrapper clearfix">
+                    <div className="col-1">
+                        <p>{this.getIntlMessage('footer.learnOnTheGo')}.</p>
+                        <h4>{this.getIntlMessage('common.anytime')}. {this.getIntlMessage('common.anywhere')}.</h4>
+                    </div>
+                    <ul className="appPlatform col-2 clearfix">
+                        <li className="ios">
+                            <a href="//itunes.apple.com/us/app/wiziq/id540018386?mt=8" target="_blank">
+                                {this.getIntlMessage('footer.availableOn')}
+                                <span>{this.getIntlMessage('common.appStore')}</span>
+                            </a>
+                        </li>
+                        <li className="android">
+                            <a href="https://play.google.com/store/apps/details?id=air.com.wiziq.ipadvc" target="_blank">
+                                {this.getIntlMessage('footer.downloadFor')}
+                                <span>{this.getIntlMessage('common.android')}</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="col-3">
+                        <figure className="clearfix">
+                            <img src={data.logo} alt={data.academyName} />
+                            <figcaption>{this.getIntlMessage('footer.poweredBy')}</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+});
+
+if (isNode) {
+    module.exports = Footer; 
+} else {
+
+    var componentName = 'cFooter_footer';
+    
+    var dataName = eval('var_' + componentName);
+    //var dakk = eval('datakk');
+    var lokk = eval('localkk')
+    var mekk = eval('messkk');
+    var fokk = eval('formkk');
+    
+    //BIFROST_APP_PROPS
+    ReactDOM.render(<Footer data={dataName} messages={mekk} formats={fokk} locales={lokk} />, document.getElementById('container_'+componentName));
+    //ReactDOM.render(<Section data={paramName} locales={LOCALES} {...MESSAGES} formats={FORMATS}/>, document.getElementById('pageWrapper'));
+    //ReactDOM.render(<Section {...BIFROST_APP_PROPS}/>, document.getElementById('pageWrapper'));
+
+}
---
+++ UI/caferno/app/components/cHeader/cHeader_header.jsx
@@ -0,0 +1,90 @@
+var isNode = typeof module !== 'undefined' && module.exports
+  , React = isNode ? require('react') : window.React
+  , ReactDOM = isNode ? require('react-dom') : window.ReactDOM
+  , ReactIntl = isNode ? require('react-intl') : window.ReactIntl;
+
+    
+var IntlMixin = ReactIntl.IntlMixin;
+var FormattedMessage = ReactIntl.FormattedMessage;
+var FormattedDate = ReactIntl.FormattedDate;
+
+var HeaderLogo = React.createClass({
+    render: function () {
+        var data = this.props.data;
+
+        return (
+            <figure className="logo">
+                <img src={data.logo} alt={data.academyName} />
+            </figure>
+        );
+    }
+});
+
+var NavigationLink = React.createClass({
+    mixins: [IntlMixin],
+    render: function () {
+        var data = this.props.data;
+        if (data.isActive) {
+            data.classes.push('active');
+        }
+        var classNames = data.classes.join(' ');
+
+        return (
+            <li className={classNames}>
+                <a href={data.url}>{this.getIntlMessage('header.headerNavigation.navigationItems.' + data.keyName)}</a>
+            </li>
+        );
+    }
+});
+
+var HeaderNavigation = React.createClass({
+    mixins: [IntlMixin],
+    render: function () {
+        var data = this.props.data;
+        var navigationItems = data.navigationItems.map(function (navigationItem) {
+            return (
+                <NavigationLink data={navigationItem} />
+            );
+        });
+
+        return (
+            <ul className="pageHeadNav clearfix">
+                {navigationItems}
+            </ul>
+        );
+    }
+});
+
+var Header = React.createClass({
+    mixins: [IntlMixin],
+    render: function () {
+        var data = this.props.data;
+        return (
+            <header id="pageHeader" className="moduleBody">
+                <div className="moduleWrapper clearfix">
+                    <HeaderLogo data={data.header} />
+                    <HeaderNavigation data={data.headerNavigation} />
+                </div>
+            </header>
+        );
+    }
+});
+
+if (isNode) {
+    module.exports = Header; 
+} else {
+
+    var componentName = 'cHeader_header';
+    
+    var dataName = eval('var_' + componentName);
+    //var dakk = eval('datakk');
+    var lokk = eval('localkk')
+    var mekk = eval('messkk');
+    var fokk = eval('formkk');
+    
+    //BIFROST_APP_PROPS
+    ReactDOM.render(<Header data={dataName} messages={mekk} formats={fokk} locales={lokk} />, document.getElementById('container_'+componentName));
+    //ReactDOM.render(<Section data={paramName} locales={LOCALES} {...MESSAGES} formats={FORMATS}/>, document.getElementById('pageWrapper'));
+    //ReactDOM.render(<Section {...BIFROST_APP_PROPS}/>, document.getElementById('pageWrapper'));
+
+}
